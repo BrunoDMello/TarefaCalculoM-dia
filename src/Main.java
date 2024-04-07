2709ea49d4b3fef339684e783ba8a2753f486d8d
BrunoDMello <bruno.ernst.mello.dev@gmail.com>
2024-04-07 19:17:01 -0300

    Subindo tarefa calculo média

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * @author bruno
+ */
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        int num1 = 9;
+        int num2 = 6;
+        int num3 = 5;
+        int num4 = 8;
+        int media = (num1+num2+num3+num4) / 4;
+
+        System.out.println("Olá, na tarefa de hoje iremos realizar o calculo da média de 4 notas");
+        System.out.println("Nota 1: " + num1);
+        System.out.println("Nota 2: " + num2);
+        System.out.println("Nota 3: " + num3);
+        System.out.println("Nota 4: " + num4);
+        System.out.println("A média dessas 4 notas é: " + media);
+    }
+}
